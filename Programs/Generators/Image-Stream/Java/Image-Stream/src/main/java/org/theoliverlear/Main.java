09c0b42878678845d051be8f8c0cfe0deefe7796
theoliverlear <sigwartholiver2@gmail.com>
2023-09-21 22:32:00 -0500

    Initial Commit

---
+++ Programs/Generators/Image-Stream/Java/Image-Stream/src/main/java/org/theoliverlear/Main.java
@@ -0,0 +1,12 @@
+package org.theoliverlear;
+
+import org.opencv.*;
+import org.opencv.core.*;
+
+public class Main {
+    public static void main(String[] args) {
+        System.loadLibrary(Core.NATIVE_LIBRARY_NAME); // opencv_java470
+        
+
+    }
+}
